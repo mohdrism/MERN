e6c350b673741dd7d686a95e03c33bf147e77760
Mohammed Rismy <mohammedrism26@gmail.com>
2026-02-20 01:13:08 +0530

    ...

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 // backend entry pint 
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+app.get('/',(req,res)=> {
+    res.send('Backend is running!');
+});
+
+app.listen(PORT, () => {
+    console.log('Server is running on http://localhost:${PORT}');
+});
