038ce2a1fcd24c854611db02f8a3656d62cfef0c
Mohammed Rismy <mohammedrism26@gmail.com>
2026-03-01 00:44:49 +0530

    Day 1 - Variables and Conditionals Practice

---
+++ Phase1_js/Fundamentals/conditionals.js
@@ -0,0 +1,13 @@
+let price = 85000;
+let quantity = 5;
+
+let totalPrice = price*quantity;
+
+if(totalPrice>100000){
+    console.log("Expensive Order");
+} 
+else{
+    console.log("Expensive Order");
+}
+
+console.log("Total price: "+ totalPrice);
---
+++ Phase1_js/Fundamentals/variables.js
@@ -0,0 +1,19 @@
+// Variables + dtat types + operators
+
+let productName = "phone";
+let price = 85000;
+let quantity = 2;
+let isAvailable = true;
+
+//cal total price
+
+let total = price*quantity;
+
+//output
+
+console.log("Product: ", productName);
+console.log("Price per unit: ", price);
+console.log("Quantity: ", quantity);
+console.log("Total price: ", total);
+console.log("Available: ", isAvailable);
+
